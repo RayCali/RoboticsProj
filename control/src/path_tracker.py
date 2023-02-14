c0f3bd0e89aad11be4c8a40aa550c2d7ce6cc906
Kevin Jamsahar <kevinjamsahar@gmail.com>
2023-02-14 10:13:56 +0100

    odom to base

--- control/src/path_tracker.py
+++ control/src/path_tracker.py
@@ -17,10 +17,6 @@ import tf
 x = 0.0
 y = 0.0
 theta = 0.0
-# marker_ID = 0
-# marker_position = Point()
-# marker_orientation = Quaternion()
-# marker_pose = Pose()
 
 class path(object):
     def __init__(self):
@@ -31,6 +27,7 @@ class path(object):
     def tracker(self,msg:PoseStamped):
         global x, y, theta
         rate = rospy.Rate(20)
+        
 
         self.cam_pose = PoseStamped()
         twist = Twist()
@@ -43,20 +40,39 @@ class path(object):
             self.cam_pose.header = msg.header
             self.cam_pose.pose = msg.pose
 
+            broadcaster = tf2_ros.TransformBroadcaster()
+            t = TransformStamped()
+  
+
             try:
-                trans_base = tfBuffer.lookup_transform("base_link", "odom", msgTime, timeout=rospy.Duration(6.0))
+                trans_base = tfBuffer.lookup_transform("base_link", "map", msgTime, timeout=rospy.Duration(6.0))
+                # print(trans_base)
                 self.goal_pose = tf2_geometry_msgs.do_transform_pose(self.cam_pose, trans_base)
-                print('jkdlsakldfjkdfalkfa')
+                print(self.goal_pose)
+                # print('jkdlsakldfjkdfalkfa')
                 # return self.goal_pose
             except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+                rospy.logerr("Failed to transform point from odom frame to base_link frame")
                 pass
             
-            print('mamke robin')
+            t.header = msg.header
+            t.header.frame_id = "base_link"
+            t.child_frame_id = "goal"
+            t.transform.translation.x = self.goal_pose.pose.position.x
+            t.transform.translation.y = self.goal_pose.pose.position.y
+            t.transform.translation.z = self.goal_pose.pose.position.z
+
+            # print('mamke robin')
 
             # print(self.goal_pose)
-
-            inc_x = self.goal_pose.pose.position.x - x
-            inc_y = self.goal_pose.pose.position.y - y
+            #Update the position
+            x = t.transform.translation.x = self.goal_pose.pose.position.x
+            y = t.transform.translation.y = self.goal_pose.pose.position.y
+            z = t.transform.translation.z = self.goal_pose.pose.position.z
+            # inc_x = self.goal_pose.pose.position.x - x
+            # inc_y = self.goal_pose.pose.position.y - y
+            inc_x = x
+            inc_y = y
             print(inc_x, inc_y)
 
             if math.sqrt(inc_x**2 + inc_y**2)<0.1:
@@ -73,7 +89,7 @@ class path(object):
                 pub_twist.publish(twist)
                 print('Moving towards the goal')
         
-
+            broadcaster.sendTransform(t)
 
 
         rate.sleep()
@@ -83,11 +99,7 @@ if __name__ == "__main__":
 
     tfBuffer = tf2_ros.Buffer()
     tflistener = tf2_ros.TransformListener(tfBuffer)
-    # tfListener = tf.TransformListener()
 
-
-    # rospy.Subscriber("/aruco/markers", MarkerArray, display_markers)
-    
     pub_duty = rospy.Publisher('/motor/duty_cycles', DutyCycles, queue_size=10)
     pub_twist = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
     try:
