0cd7a1f6fb52358729e439d2fde0d05873035ae2
Magnus TIbbe <magnus.tibbe@gmail.com>
2023-02-15 18:31:17 +0100

    Node takes input image, gets bounding boxes and overlays them on the image and publishes. It should also get the pos of the detected object but there are still difficulties obtaining the depth of the image points

--- detection/src/detection.py
+++ detection/src/detection.py
@@ -8,6 +8,8 @@ from sensor_msgs.msg import Image
 from geometry_msgs.msg import PoseStamped
 from .....RoboticsProj_VisionModel import utils, detector
 
+FOCAL_LENGTH = 1.93/1000 # focal lenth in m
+BASELINE = 50/1000 # baseline in m
 
 def imageCB(msg):
     # msg is of type Image, convert to torch tensor
@@ -17,9 +19,29 @@ def imageCB(msg):
     inference = detectionModel(torch_image) # size: (15,20,5)
 
     bbs = detectionModel.decode_output(inference, threshold=0.7)
-
     
-    utils.add_bounding_boxes()
+    # only one image, so iterate over all bbs in that image
+    # to extract their position and overlay them over the image
+    for bb in bbs[0]:
+        x = bb["x"]
+        y = bb["y"]
+        width = bb["width"]
+        height = bb["height"]
+
+        # extract center position of box
+        center_x = x+int(width/2)
+        center_y = y-int(height/2)
+
+        # overlay box on image
+        X = np.arange(x, x+width)
+        Y = np.arange(y, y-height, step=-1)
+        np_image[Y,X,0] = 255
+        np_image[Y,X,1] = 0
+        np_image[Y,X,2] = 0
+    
+    pubImg = rnp.msgify(Image,np_image)
+
+    imgPub.publish(pubImg)
     
 
 
@@ -28,6 +50,7 @@ if __name__=="__main__":
 
     imageSub = rospy.Subscriber("/camera/color/image_raw", Image, imageCB)
     posePub = rospy.Publisher("/detection/pose", PoseStamped, queue_size=10)
+    imgPub = rospy.Publisher("detection/overlaid_bbs", Image, queue_size=10)
 
     # Load model
     detectionModel = utils.load_model(detector.Detector(),"~/RoboticsProj_VisionModel/models/det_2023-02-13_17-20-07-496421.pt", device="gpu")
