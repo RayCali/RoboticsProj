00f2ff7f452d9a7c8142d19b644d57036b7016bb
Staffan Konstholm robotikprojekt <stakon@kth.se>
2023-03-16 14:44:06 +0100

    It is now time to write a parser algorithm that deciphers a message

--- mapping_and_planning/src/gridmapping.py
+++ mapping_and_planning/src/gridmapping.py
@@ -1,18 +1,26 @@
 from gridobject import GridObject
 import matplotlib.pyplot as plt
-import numpy as np
+from typing import List
 
 class Mapper:
     def __init__(self) -> None:
-        self.go = self.__getGridObject()
+        self.__setGridObject()
     
-    def __getGridObject(self, resolution: int = 0.01) -> GridObject:
-        return GridObject(resolution)
+    def __setGridObject(self, resolution: int = 0.01) -> GridObject:
+        self.go = GridObject(resolution)
     
     def doAnimate(self):
         x = self.go.getPILObject()
-        print(x)
-        print(x.shape)
-        
         plt.imshow(x)
         plt.show()
+    
+    def doInterpreteMSG(self, msg) -> None:
+        positions: List[List[int]] = []
+        id = "UNK"
+        # TODO: 
+        # 1. add functionality that deciphers a msg into 
+        # a list of positions within the grid
+        # 2. figure out what type of object it is
+        
+        self.go.doPlace(positions=positions, id = id)
+        return
--- mapping_and_planning/src/gridobject.py
+++ mapping_and_planning/src/gridobject.py
@@ -1,20 +1,33 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from typing import List
+
 class GridObject:
     def __init__(self, resolution: int) -> None:
         L = 10
         N = int(L / resolution) #ir stands for Internal Representation
         self.__ir = np.array([[(0, 255, 255, 255) for i in range(N)] for i in range(N)], dtype=np.uint8)
+        self.__id2info = {
+            "UNK": (0, 255, 255, 255),
+            "OBS": (1, 0, 0, 0),
+            "BOX": (2, 0, 0, 255),
+            "TOY": (3, 0, 255, 0),
+        }
         self.__drawBox()
-    
+
     def __drawBox(self):
+        l = []
         for i in range(500,600,1):
             for ii in range(500,600,1):
-                self.__ir[i,ii] = (1,0,0,0)
-
+                l.append([i,ii])
+                #self.__ir[i,ii] = (1,0,0,0)
+        self.doPlace(positions = l, id="OBS")
 
     def getPILObject(self) -> np.array:
         return self.__ir[:,:,1:]
-
+    
+    def doPlace(self, positions: List[List[int]], id = "UNK"):
+        for p in positions:
+            self.__ir[p[0], p[1]] = self.__id2info[id]
     
 
