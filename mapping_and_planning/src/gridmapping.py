780a9464a14e677e1933bfc239656fa150295709
Staffan Konstholm robotikprojekt <stakon@kth.se>
2023-03-20 10:44:31 +0100

    Added tf2->grid conversion, need classification in header

--- mapping_and_planning/src/gridmapping.py
+++ mapping_and_planning/src/gridmapping.py
@@ -1,10 +1,12 @@
 from gridobject import GridObject
 import matplotlib.pyplot as plt
 from typing import List
+from geometry_msgs.msg import TransformStamped
 
 class Mapper:
     def __init__(self) -> None:
         self.__setGridObject()
+        self.frames_dict_keys = []
     
     def __setGridObject(self, resolution: int = 0.01) -> GridObject:
         self.go = GridObject(resolution)
@@ -14,13 +16,17 @@ class Mapper:
         plt.imshow(x)
         plt.show()
     
-    def doInterpreteMSG(self, msg) -> None:
+    def doInterpreteMSG(self, msg:TransformStamped, id:str="UNK") -> None:
         positions: List[List[int]] = []
         id = "UNK"
         # TODO: 
-        # 1. add functionality that deciphers a msg into 
-        # a list of positions within the grid
         # 2. figure out what type of object it is
-        
+        x = msg.transform.translation.x
+        y = msg.transform.translation.y
+        positions = [
+            [x,y],
+            ]
         self.go.doPlace(positions=positions, id = id)
-        return
+    
+    def keyLogged(self, key: str) -> bool:
+        return self.frames_dict_keys[key] is not None
