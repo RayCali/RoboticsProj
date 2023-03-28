011b18c0324b1cf23820bb349cda1679ceb63153
Staffan Konstholm robotikprojekt <stakon@kth.se>
2023-03-28 15:45:24 +0200

    Created a memory package

---
+++ memory/src/main.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+import rospy
+import numpy as np
+import tf2_ros
+from nav_msgs.msg import OccupancyGrid
+from geometry_msgs.msg import Pose, Point, Quaternion, PoseStamped
+from sensor_msgs.msg import LaserScan
+import matplotlib.pyplot as plt
+import tf_conversions
+from objects import Plushie, Cube, Ball, Box, Movable
+from typing import Dict
+
+
+class Mem:
+    def __init__(self):
+        self.objects: Dict[Movable]= {}
+        self.plushies: Dict[Plushie] = {}
+        self.cubes: Dict[Cube] = {}
+        self.balls: Dict[Ball] = {}
+        self.boxes: Dict[Box] = {}
+        self.id2Object = {
+           0 : "Binky",
+           1 : "Hugo",
+           2 : "Slush",
+           3 : "Muddles",
+           4 : "Kiki",
+           5 : "Oakie",
+           6 : "Cube",
+           7 : "Ball",
+           8 : "Box",
+        }
+
+    def __putObject(self, pose: PoseStamped, id: int):
+        id = int(id)
+        object: Movable
+        objectType = None
+        correctDict = None
+        if id in [0, 1, 2, 3, 4, 5]:
+            objectType = Plushie
+            correctDict = self.plushies
+        elif id==6:
+            objectType = Cube
+            correctDict= self.cubes
+        elif id==7:
+            objectType = Ball
+            correctDict= self.balls
+        elif id==8:
+            objectType = Box
+            correctDict = self.boxes
+        else:
+            raise Exception("Invalid object ID: " % str(id))
+        name = self.id2Object[id] + "_" + str(objectType.count)
+        objectType.count += 1
+        object = objectType(pose=pose, name=name)
+        self.objects[name] = object
+        correctDict[name] = object
+
+
+
+            
+
+
+if __name__ == "__main__":
+
+    rospy.init_node("memory")
+    m = Mem()
+    rospy.spin()    
---
+++ memory/src/objects.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+import rospy
+import numpy as np
+import tf2_ros
+from nav_msgs.msg import OccupancyGrid
+from geometry_msgs.msg import Pose, Point, Quaternion, PoseStamped
+from sensor_msgs.msg import LaserScan
+import matplotlib.pyplot as plt
+import tf_conversions
+
+class Movable:
+    def __init__(self, pose: PoseStamped, name: str) -> None:
+        self.x = pose.pose.position.x
+        self.y = pose.pose.position.y 
+        self.stamp = pose.header.stamp
+        self.name = None
+
+class Toy(Movable):
+    count = 0
+    def __init__(self, pose: PoseStamped, name: str) -> None:
+        super().__init__(pose, name)
+        self.inBox = False
+
+class Plushie(Toy):
+    def __init__(self, pose: PoseStamped, name: str) -> None:
+        super().__init__(pose, name)
+        self.inBox = False
+class Cube(Toy):
+    def __init__(self, pose: PoseStamped, name: str) -> None:
+        super().__init__(pose, name)
+        self.inBox = False
+
+class Ball(Toy):
+    def __init__(self, pose: PoseStamped, name: str) -> None:
+        super().__init__(pose, name)
+        self.inBox = False
+
+class Box(Movable):
+    count = 0
+    def __init__(self, pose: PoseStamped, name: str) -> None:
+        super().__init__(pose, name)
+        self.hasArucoMarker = False
